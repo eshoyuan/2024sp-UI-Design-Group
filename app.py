0e97ea4f293483a3207911a291db605c52bb2394
Yuehui Ruan <68934756+Ricky-lab@users.noreply.github.com>
2024-04-16 21:21:22 -0400

    init of the basic framework

--- app.py
+++ app.py
@@ -3,12 +3,14 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
+@app.route('/home')
 def home():
     return render_template('home.html')
 
 @app.route('/origin')
 def original():
     return render_template('origin.html')  
+
 @app.route('/spread')
 def spread():
     return render_template('spread.html') 
